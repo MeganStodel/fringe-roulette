b60ccc24983c016acc400e0208011bd09410c6f1
Megan Stodel <megjcs@gmail.com>
2019-06-19 23:53:29 +0100

    Combine data and clean obvious issues

---
+++ data-prep.R
@@ -0,0 +1,31 @@
+library(data.table)
+
+all_files <- list.files(path = "./csvs", pattern = ".csv")
+
+all_files <- paste0("./csvs/", all_files)
+
+l <- lapply(all_files, fread)
+
+fringe_shows <- rbindlist(l)
+
+fringe_shows <- unique(fringe_shows)
+
+
+### Cleaning
+
+fringe_shows[Title == '="21"', Title := '21']
+
+fringe_shows <- fringe_shows[!Title %like% "CANCELLED"]
+
+fringe_shows[, names(fringe_shows) := lapply(.SD, function(x) gsub("â€™", "'", x))]
+
+fringe_shows[, names(fringe_shows) := lapply(.SD, function(x) gsub("â€“", "-", x))]
+
+fringe_shows[, names(fringe_shows) := lapply(.SD, function(x) gsub("Ã¨", "è", x))]
+
+fringe_shows[, names(fringe_shows) := lapply(.SD, function(x) gsub("â„¢", "™", x))]
+
+fringe_shows[, names(fringe_shows) := lapply(.SD, function(x) gsub("â€¦", "...", x))]
+
+fringe_shows[, names(fringe_shows) := lapply(.SD, function(x) gsub("Ã©", "é", x))]
+
